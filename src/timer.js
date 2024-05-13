d63a24c1fdc8556600b44d6e2b734dd119429878
amirmmd1386 <hajamir1386.0@gmail.com>
2024-05-13 11:35:11 -0700

    the milli second

--- src/timer.js
+++ src/timer.js
@@ -4,11 +4,11 @@ var interval;
 class Timer extends React.Component {
     constructor() {
         super();
-        this.state = ({ hour: 0, min: 0, sec: 0 })
+        this.state = ({ hour: 0, min: 0, sec: 0, milli: 0 })
     }
 
     resetTime = () => {
-        this.setState({ hour: 0, min: 0, sec: 0 })
+        this.setState({ hour: 0, min: 0, sec: 0, milli: 0 })
         clearInterval(interval);
     }
     clearTime = () => {
@@ -16,14 +16,17 @@ class Timer extends React.Component {
     }
     startTime = () => {
         interval = setInterval(() => {
-            this.setState({ sec: this.state.sec + 1 })
-        }, 1000)
+            this.setState({ milli: this.state.milli + 1 })
+        }, 10)
     }
     componentDidUpdate() {
-        if (this.state.sec === 59) {
-            this.setState({ min: this.state.min + 1, sec: 0 })
-            if (this.state.min === 59) {
-                this.setState({ min: 0, hour: this.state.hour + 1 })
+        if (this.state.milli === 100) { 
+            this.setState({ sec: this.state.sec + 1, milli: 0 })
+            if (this.state.sec === 59) {
+                this.setState({ min: this.state.min + 1, sec: 0 })
+                if (this.state.min === 59) {
+                    this.setState({ min: 0, hour: this.state.hour + 1 })
+                }
             }
         }
     }
@@ -31,9 +34,10 @@ class Timer extends React.Component {
         let h = this.state.hour.toString().padStart(2, '0')
         let m = this.state.min.toString().padStart(2, '0')
         let s = this.state.sec.toString().padStart(2, '0')
+        let mi = this.state.milli.toString().padStart(2, '0')
         return (
             <>
-                <div className='timer'><p>{`${h}:${m}:${s}`}</p></div>
+                <div className='timer'><p>{`${h}:${m}:${s}:${mi}`}</p></div>
                 <div className='buttonDiv'>
                     <button onClick={this.startTime}>Start</button>
                     <button onClick={this.clearTime}>Stop</button>
